02e6554b2df15d0294ba1432037e782f138dd225
Lidor Cohen <lidorcohen.p1@gmail.com>
2025-11-09 21:28:40 +0200

    add: base message routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/api/auth/signup", (req, res) => {
-  res.send("signup");
-});
-
-app.get("/api/auth/login", (req, res) => {
-  res.send("login");
-});
-
-app.get("/api/auth/logout", (req, res) => {
-  res.send("logout");
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
