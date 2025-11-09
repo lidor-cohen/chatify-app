4886e58db19e854fa806a52f394bc6df8d655895
Lidor Cohen <lidorcohen.p1@gmail.com>
2025-11-09 21:21:41 +0200

    init frontend & backend

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/api/auth/signup", (req, res) => {
+  res.send("signup");
+});
+
+app.get("/api/auth/login", (req, res) => {
+  res.send("login");
+});
+
+app.get("/api/auth/logout", (req, res) => {
+  res.send("logout");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
