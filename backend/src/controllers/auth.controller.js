b278ef46c708647cc1268fd6ca760bb00cebd134
Lidor Cohen <lidorcohen.p1@gmail.com>
2025-11-10 02:05:05 +0200

    (backend) add: signup controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,17 +1,14 @@
-import User from "../models/User";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+import { generateToken } from "../lib/utils.js";
 
 export const signup = async (req, res) => {
-  const { fullname, email, password } = req.body;
+  const { fullName, email, password } = req.body;
 
   try {
-    if (!fullname || !email || !password) {
+    if (!fullName || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
-    }
-
-    if (password.length < 6) {
-      return res
-        .status(400)
-        .json({ message: "Password must be at least 6 characters" });
     }
 
     if (password.length < 6) {
@@ -22,11 +19,37 @@ export const signup = async (req, res) => {
 
     const emailRegex =
       /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-    if (!email.test(emailRegex)) {
+    if (!emailRegex.test(email)) {
       return res.status(400).json({ message: "Invalid email format!" });
     }
 
     const user = await User.findOne({ email });
     if (user) return res.status(400).json({ message: "Email already in use!" });
-  } catch (error) {}
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      return res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      return res.send(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.error("Error in signup controller: " + error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
