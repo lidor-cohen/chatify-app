aceb5373ef0fab2549e65f1c80d32236b8b168e4
Lidor Cohen <lidorcohen.p1@gmail.com>
2025-11-10 21:00:22 +0200

    (backend) add: message routes and controllers

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,19 @@
 import express from "express";
+import {
+  getAllContacts,
+  getMessagesByUserId,
+  sendMessage,
+  getAllChatPartners,
+} from "../controllers/message.controller.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
+router.use(arcjetProtection, protectRoute);
 
-router.get("/send", (req, res) => {
-  res.send("send");
-});
+router.get("/contacts", getAllContacts);
+router.get("/chats", getAllChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
