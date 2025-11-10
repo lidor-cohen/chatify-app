76ee0ef4e407f5ad5a0c81879ef24891f328bb07
Lidor Cohen <lidorcohen.p1@gmail.com>
2025-11-10 17:42:25 +0200

    (backend) add: arcjet protection against bots, spoofed bots, rate limiting and common attacks

---
+++ backend/src/lib/arcjet.js
@@ -0,0 +1,27 @@
+import arcjet, { shield, detectBot, slidingWindow } from "@arcjet/node";
+import { ENV } from "./env.js";
+
+const aj = arcjet({
+  key: ENV.ARCJET_KEY,
+  rules: [
+    // Shield protects your app from common attacks e.g. SQL injection
+    shield({ mode: "LIVE" }),
+    // Create a bot detection rule
+    detectBot({
+      mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
+      // Block all bots except the following
+      allow: [
+        // See the full list at https://arcjet.com/bot-list
+        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+      ],
+    }),
+    // Create a token bucket rate limit. Other algorithms are supported.
+    slidingWindow({
+      mode: "LIVE",
+      max: 100,
+      interval: 60,
+    }),
+  ],
+});
+
+export default aj;
