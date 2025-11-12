6efa7c927b8acf2ab4ff55ed83aacfc47571d668
Lidor Cohen <lidorcohen.p1@gmail.com>
2025-11-12 21:17:58 +0200

    (frontend) fix: typo

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -5,7 +5,7 @@ import { useAuthStore } from "../store/useAuthStore";
 import ChatHeader from "./ChatHeader";
 import NoChatHistoryPlaceholder from "./NoChatHistoryPlaceholder";
 import MessageInput from "./MessageInput";
-import MessagesloadingSkeleton from "./MessagesLoadingSkeleton";
+import MessagesLoadingSkeleton from "./MessagesLoadingSkeleton";
 
 function ChatContainer() {
   const { selectedUser, getMessagesByUserId, messages, isMessagesLoading } =
@@ -61,7 +61,7 @@ function ChatContainer() {
             <div ref={messageEndRef}></div>
           </div>
         ) : isMessagesLoading ? (
-          <MessagesloadingSkeleton />
+          <MessagesLoadingSkeleton />
         ) : (
           <NoChatHistoryPlaceholder name={selectedUser.fullName} />
         )}
